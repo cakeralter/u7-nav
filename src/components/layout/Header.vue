e62501a83f2aa0f0ab7eaec27b00e64f4db3c72c
wangyl <wangyl@hfwedo.com>
2020-03-22 20:45:17 +0800

    Header & 搜索框

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="header">
+    <div class="logo-warpper">
+      <span class="site-name">有趣导航</span>
+    </div>
+    <div class="setting-warpper">
+      <span class="setting-link">登录</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+#header {
+  height: 32px;
+  border-bottom: 1px solid #ebebeb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #f7f7f7;
+}
+.logo-warpper,
+.setting-warpper {
+  height: 32px;
+  line-height: 32px;
+}
+.logo-warpper {
+  padding-left: 15px;
+}
+.site-name {
+  font-size: 13px;
+  color: #555;
+  font-weight: bold;
+}
+.setting-link {
+  font-size: 12px;
+  color: #555;
+  margin-right: 19px;
+  display: inline-block;
+  cursor: pointer;
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/layout/Main.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="main">
+    <main>
+      <div class="top">
+        <div class="search-warpper">
+          <img
+            src="../../assets/nav_google.png"
+            alt="Google"
+            class="search-nav-img left"
+          >
+          <input
+            type="text"
+            class="search-ipt-txt"
+            placeholder="点击左侧图标可选择搜索引擎；按回车搜索"
+          >
+          <img
+            src="../../assets/nav_btn.png"
+            alt="搜索"
+            class="search-nav-img right"
+          >
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+#main {
+  min-height: calc(100vh - 95px);
+  padding: 30px 32px 32px;
+}
+main {
+  width: 1024px;
+  margin: auto;
+}
+.top {
+  padding: 0 25px;
+}
+.search-warpper {
+  padding-bottom: 60px;
+  width: 736px;
+  min-height: 50px;
+  margin: auto;
+  position: relative;
+}
+.search-nav-img {
+  position: absolute;
+  top: 10px;
+  width: 30px;
+}
+.left {
+  left: 10px;
+}
+.right {
+  right: 10px;
+  cursor: pointer;
+}
+.search-ipt-txt {
+  width: calc(100% - 102px);
+  font-size: 15px;
+  box-shadow: 0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  border: 1px solid #e6e6e6;
+  line-height: 50px;
+  padding: 0 50px;
+  color: #999;
+}
+.search-ipt-txt:focus {
+  border: 1px solid #2188ff;
+  box-shadow: 0 0 5px 0px rgba(71, 158, 245, 0.5);
+}
+</style>
